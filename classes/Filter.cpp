267cf899b4a9b804ffe5732b34682b846d3ac018
Nils Werner <nils.werner@gmail.com>
2012-02-21 11:44:46 +0100

    Sigmas und Omegas berechnen

--- classes/Filter.cpp
+++ classes/Filter.cpp
@@ -35,11 +35,22 @@ Filter::Filter(float length = 0.65) {
 
 void Filter::createPoles() {
 	int i;
+	double gamma, sigma, omega;
 	vector<float> sigmas(this->filters);
 	vector<float> omegas(this->filters);
 
 	for(i = 0; i < this->filters; i++) {
-		sigmas.push_back(1);
-		omegas.push_back(2);
+		gamma = i * ( M_PI / this->l );
+		sigma = (1 / (2 * this->rho * this->A) ) * (this->d3 * pow(gamma,2) - this->d1);
+		omega = sqrt(
+				  ( (this->E * this->I) / (this->rho * this->A) )
+				- ( (pow(this->d3, 2) / pow(2 * this->rho * this->A, 2)) * pow(gamma, 4) )
+				+ ( (this->Ts / (this->rho * this->A) ) * pow(gamma, 2) )
+				+ ( pow(this->d1 / (2 * this->rho * this->A), 2) )
+			);
+		
+		sigmas.push_back(sigma);
+		omegas.push_back(omega);
+		
 	}
 }
--- classes/Filter.h
+++ classes/Filter.h
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <iostream>
 #include <vector>
+#include <math.h>
 
 class Filter {
 
