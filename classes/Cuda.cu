b74b41f53c9917006d4e6c4eae9a7e57990df691
Nils Werner <nils.werner+lms@gmail.com>
2012-03-26 14:24:02 +0200

    Tippfehler in Cuda.cu Funktionen

--- classes/Cuda.cu
+++ classes/Cuda.cu
@@ -1,32 +1,32 @@
 #include "Cuda.h"
 
-Cuda::copyToDevice() {
+void Cuda::copyToDevice() {
 	/*
 	CUDA_SAFE_CALL(cudaMalloc((void**)&d_A.elements, size));
 	CUDA_SAFE_CALL(cudaMemcpy(d_A.elements, A.elements, size, cudaMemcpyHostToDevice));
 	*/
 }
 
-Cuda::copyToHost() {
+void Cuda::copyToHost() {
 	/*
 	CUDA_SAFE_CALL(cudaMalloc((void**)&d_B.elements, size));
 	CUDA_SAFE_CALL(cudaMemcpy(d_B.elements, B.elements, size, cudaMemcpyDeviceToHost));
 	*/
 }
 
-Cuda::malloc() {
+void Cuda::malloc() {
 	/*
 	CUDA_SAFE_CALL(cudaMalloc((void**)&d_C.elements, size));
 	*/
 }
 
-Cuda::free() {
+void Cuda::free() {
 	/*
 	cudaFree(d_A.elements);
 	*/
 }
 
-Cuda::invoke() {
+void Cuda::invoke() {
 	/*
 	dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE);
 	dim3 dimGrid(B.width / dimBlock.x, A.height / dimBlock.y);
