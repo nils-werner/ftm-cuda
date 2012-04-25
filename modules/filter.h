d6891c20884932c94051f4e0ca713faee767833b
Nils Werner <nils.werner+lms@gmail.com>
2012-04-25 12:52:22 +0200

    Filter mit nvcc kompilieren

--- modules/filter.h
+++ modules/filter.h
@@ -4,6 +4,10 @@
 #include "modules/matrix.h"
 #include <math.h>
 #include <sndfile.h>
+#include "cuda/matrixmultiply.kernel.h"
+#include "cuda/blockdiagmatrixmultiply.kernel.h"
+#include <cuda_runtime.h>
+#include <cutil.h>
 
 int filter(float);
 void initializeCoefficients(float);
