6a35b215d32df00eeafd6f831a13bba16bb7b44b
Nils Werner <nils.werner+lms@gmail.com>
2012-05-02 14:49:53 +0200

    Dokumentation fuer main methode

--- iirfilter.cu
+++ iirfilter.cu
@@ -1,5 +1,12 @@
 #include "iirfilter.h"
 
+/**
+ * Main method required in all C programs. Simply calls filter with a predefined string length
+ *
+ * @param void
+ * @return int 0
+ */
+
 int main() {
 	filter(0.65);
 	return 0;
