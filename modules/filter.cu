e38a97ca59d3208bb147d26c7ee61a583809765f
Nils Werner <nils.werner+lms@gmail.com>
2012-05-30 18:16:05 +0200

    Blockdiagonale Matrix Auf GPU mit optimiertem Kernel berechnen

--- modules/filter.cu
+++ modules/filter.cu
@@ -253,7 +253,7 @@ void createBlockprocessingMatricesGPU() {
 		MatrixMultiplyKernel<<<dimGridCA, dimBlockCA, 1, streams[0]>>>(device_MatrixC, *pointer_device_MatrixAp_read, *pointer_device_MatrixCA_line_write);
 		// m_multiply(&MatrixC, pointer_MatrixAp, &MatrixCA_line);
 
-		MatrixMultiplyKernel<<<dimGridA, dimBlockA, 1, streams[2]>>>(*pointer_device_MatrixAp_read, device_MatrixA, *pointer_device_MatrixAp_write);
+		BlockDiagMatrixMultiplyKernel<<<dimGridA, dimBlockA, 1, streams[2]>>>(*pointer_device_MatrixAp_read, device_MatrixA, *pointer_device_MatrixAp_write, 2);
 		// m_multiplyblockdiag(pointer_MatrixAp, &MatrixA, pointer_MatrixAp_tmp, 2);
 
 		cudaThreadSynchronize();
