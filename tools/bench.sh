26f449a420510e49fbf2f1f89b9a1bced51d392e
Nils Werner <nils.werner+lms@gmail.com>
2012-05-31 12:20:48 +0200

    Matrixmodes in bench.sh

--- tools/bench.sh
+++ tools/bench.sh
@@ -3,16 +3,19 @@
 unset IFS
 
 MODES=( gpu cpu )
+MATRIXMODES=( gpu cpu )
 TRIES=( 1 )
 
 case $1 in
 	"filters")
 		MODES=( gpu )
+		MATRIXMODES=( gpu )
 		FILTERS=( 30 90 150 210 270 330 390 450 500 550 600 650 700 750 )
 		CHUNKSIZES=( 100 )
 		;;
 	"chunks")
 		MODES=( gpu )
+		MATRIXMODES=( gpu )
 		FILTERS=( 30 )
 		CHUNKSIZES=( 25 50 75 100 125 150 175 200 225 250 )
 		;;
@@ -32,20 +35,29 @@ echo "<benchmark>" >> bench.xml
 
 for mode in ${MODES[@]}
 do
-	for filter in ${FILTERS[@]}
+	for matrixmode in ${MATRIXMODES[@]}
 	do
-		for chunk in ${CHUNKSIZES[@]}
+		for filter in ${FILTERS[@]}
 		do
-			for try in ${TRIES[@]}
+			for chunk in ${CHUNKSIZES[@]}
 			do
-				if [ $mode == "gpu" ]; then
-					modeswitch="-g"
-				else
-					modeswitch=""
-				fi
+				for try in ${TRIES[@]}
+				do
+					if [ $mode == "gpu" ]; then
+						modeswitch="-g"
+					else
+						modeswitch=""
+					fi
 
-				echo "./build/iirfilter $modeswitch -f $filter -c $chunk -x"
-				./build/iirfilter $modeswitch -f $filter -c $chunk -x >> bench.xml;
+					if [ $matrixmode == "gpu" ]; then
+						matrixmodeswitch="-b"
+					else
+						matrixmodeswitch=""
+					fi
+
+					echo "./build/iirfilter $modeswitch $matrixmodeswitch -f $filter -c $chunk -x"
+					./build/iirfilter $modeswitch $matrixmodeswitch -f $filter -c $chunk -x >> bench.xml;
+				done
 			done
 		done
 	done
