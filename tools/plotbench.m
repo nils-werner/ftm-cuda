4b65919c58c1889e6a03828bd0291d4bb08e442d
Nils Werner <nils.werner@gmail.com>
2012-06-12 17:09:31 +0200

    Vielfache der Abspielgeschwindigkeit

--- tools/plotbench.m
+++ tools/plotbench.m
@@ -1,8 +1,11 @@
+clear;
+
 M = importdata('bench.csv', ';', 1);
 
 tries = 5;
 nrblocksizes = 25;
 nrfilters = length(M.data)/(nrblocksizes*4*tries);
+samplerate = 44100;
 
 filters = 1;
 blocklength = 2;
@@ -19,12 +22,12 @@ gpucpu = intersect(query(M,1,'gpu'), query(M,2,'cpu'));
 cpugpu = intersect(query(M,1,'cpu'), query(M,2,'gpu'));
 cpucpu = intersect(query(M,1,'cpu'), query(M,2,'cpu'));
 
-z = get(M, overall, gpugpu);
+z = get(M, roundtrip, gpugpu);
 z = blkproc(z, [tries 1], @mean);
 
 x = z(:,1);
 y = z(:,2);
-z = z(:,3)
+z = z(:,3);
 
 x = reshape(x, [], nrfilters);
 x = x(1,:)';
@@ -32,7 +35,11 @@ y = reshape(y, nrblocksizes, []);
 y = y(:,1);
 z = reshape(z, nrblocksizes, []);
 
+z = z/1000000; % Microsekunden -> Sekunden
+z = 1./(z./(repmat(y, 1, nrfilters)./44100));
+
 surf(x,y,z)
 
 xlabel('Filters');
 ylabel('Blocksize');
+zlabel('Multiples of Playbackspeed');
