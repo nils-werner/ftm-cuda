86f703b18584de9a2e72b22ff5ab6e0740a2a6bb
Nils Werner <nils.werner@gmail.com>
2012-06-12 14:48:21 +0200

    Matlab zeigt erste Graphen

--- tools/plotbench.m
+++ tools/plotbench.m
@@ -1,15 +1,22 @@
-% M = importdata('bench.csv', ';', 1);
+M = importdata('bench.csv', ';', 1);
+
+tries = 5;
+nrblocksizes = 25;
+nrfilters = length(M.data)/nrblocksizes;
 
 turnaround = 4;
 roundtrip = 5;
 overall = 6;
 
-query = inline('find(ismember(M.textdata(:,col), search)==1)','M','col','search');
-get = inline('M.data(val,col)','M','col','val');
+query = inline('find(ismember(M.textdata(:,col), search)==1)-1','M','col','search');
+get = inline('M.data(val,[1 2 col])','M','col','val');
 
 gpugpu = intersect(query(M,1,'gpu'), query(M,2,'gpu'));
 gpucpu = intersect(query(M,1,'gpu'), query(M,2,'cpu'));
 cpugpu = intersect(query(M,1,'cpu'), query(M,2,'gpu'));
 cpucpu = intersect(query(M,1,'cpu'), query(M,2,'cpu'));
 
-get(M, roundtrip, gpugpu)
+x = get(M, overall, gpugpu)
+x = blkproc(x, [tries 1], @mean)
+x = reshape(x(:,3), nrblocksizes, [])
+surf(x)
