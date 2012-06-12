7e40eaa10dafa0785dd554c8aa8097508fee37d5
Nils Werner <nils.werner@gmail.com>
2012-06-12 14:59:41 +0200

    Achsen richtig anzeigen

--- tools/plotbench.m
+++ tools/plotbench.m
@@ -2,8 +2,11 @@ M = importdata('bench.csv', ';', 1);
 
 tries = 5;
 nrblocksizes = 25;
-nrfilters = length(M.data)/nrblocksizes;
+nrfilters = length(M.data)/(nrblocksizes*4*tries);
 
+filters = 1;
+blocklength = 2;
+samples = 3;
 turnaround = 4;
 roundtrip = 5;
 overall = 6;
@@ -16,7 +19,20 @@ gpucpu = intersect(query(M,1,'gpu'), query(M,2,'cpu'));
 cpugpu = intersect(query(M,1,'cpu'), query(M,2,'gpu'));
 cpucpu = intersect(query(M,1,'cpu'), query(M,2,'cpu'));
 
-x = get(M, overall, gpugpu)
-x = blkproc(x, [tries 1], @mean)
-x = reshape(x(:,3), nrblocksizes, [])
-surf(x)
+z = get(M, overall, gpugpu);
+z = blkproc(z, [tries 1], @mean);
+
+x = z(:,1);
+y = z(:,2);
+z = z(:,3)
+
+x = reshape(x, [], nrfilters);
+x = x(1,:)';
+y = reshape(y, nrblocksizes, []);
+y = y(:,1);
+z = reshape(z, nrblocksizes, []);
+
+surf(x,y,z)
+
+xlabel('Filters');
+ylabel('Blocksize');
