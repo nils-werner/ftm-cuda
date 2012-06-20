f01f8300f9b67f6a9ef66f5c8fb70c7f67546204
Nils Werner <nils.werner+lms@gmail.com>
2012-06-20 17:48:53 +0200

    Loops-Berechnung angepasst

--- tools/bench.sh
+++ tools/bench.sh
@@ -66,7 +66,7 @@ if [ -n "$MESSAGE" ]; then
 	MESSAGE=-$MESSAGE
 fi
 
-total=$(expr ${#MODES[@]} \* ${#MATRIXMODES[@]} \* ${#FILTERS[@]} \* ${#CHUNKSIZES[@]} \* ${#TRIES[@]})
+total=$(expr ${#MODES[@]} \* ${#MATRIXMODES[@]} \* ${#FILTERS[@]} \* ${#CHUNKSIZES[@]} \* ${#BLOCKSIZES[@]} \* ${#TRIES[@]})
 i=0
 
 START=$(date +%s)
